323733adc1bd0f6dd65fb13babe3f6e8525059a5
mandeepk227 <mandeep227@gmail.com>
2020-11-18 20:00:00 -0600

    first commit

---
+++ test/users.js
@@ -0,0 +1,16 @@
+import supertest from "supertest";
+const request = supertest('https://gorest.co.in/public/');
+const TOKEN = 'f163e665de46756702766387b5efd3ddd4c5d96453fbf64c521cb3e6e87e767f'
+
+import {expect} from 'chai';
+
+describe('Users', () => {
+
+    it('GET /Users', () => {
+        request.get(`v2/users?access-token=${TOKEN}`).end((err,res) => {
+            expect(res).to.not.be.empty;
+            console.log(err)
+            console.log(res.body)
+        })
+    });
+});
