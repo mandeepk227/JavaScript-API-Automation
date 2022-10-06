48b1fb2f64f1d9dd9b71a68b6e39b5b73dfbb138
mandeepk227 <mandeep227@gmail.com>
2022-10-07 00:24:28 +0530

    added done

--- test/users.js
+++ test/users.js
@@ -4,13 +4,14 @@ const TOKEN = 'f163e665de46756702766387b5efd3ddd4c5d96453fbf64c521cb3e6e87e767f'
 
 import {expect} from 'chai';
 
-describe('Users', () => {
+describe('Users',  () => {
 
-    it('GET /Users', () => {
-        request.get(`v2/users?access-token=${TOKEN}`).end((err,res) => {
-            expect(res).to.not.be.empty;
+    it('GET /Users', (done) => {
+         request.get(`v2/users?access-token=${TOKEN}`).end((err,res) => {
+           expect(res).to.not.be.empty;
             console.log(err)
             console.log(res.body)
+            done();
         })
     });
 });
