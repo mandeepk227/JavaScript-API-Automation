e3c756da0030cc8a41ba87d51ab0ea619cd3cae6
mandeepk227 <mandeep227@gmail.com>
2022-10-09 07:32:48 +0530

    added posts spec file

---
+++ test/posts.js
@@ -0,0 +1,27 @@
+import supertest from "supertest";
+const request = supertest('https://gorest.co.in/public/');
+const TOKEN = 'f163e665de46756702766387b5efd3ddd4c5d96453fbf64c521cb3e6e87e767f'
+
+import {expect} from 'chai';
+
+describe('user posts', () => {
+
+    it('POST', () => {
+
+        const data = {
+            "user_id": 310,
+            "title": "My new post",
+            "body": "Nwe post"
+          }
+        return request
+        .post('v2/posts')
+        .set('Authorization', `Bearer ${TOKEN}`)
+        .send(data)
+        .then((res) => {
+            console.log(res.body)
+            expect(res.body.user_id).to.be.eq(data.user_id)
+        })
+    })
+
+    
+})
